36c1eea96b7267327ef2f79c1ab0d9b02de3569e
Hernan Y.Ke <yuriqiao@gmail.com>
2016-02-22 09:43:27 -0500

    f.valid form.value form.controls

--- dev/tempform.component.ts
+++ dev/tempform.component.ts
@@ -9,17 +9,22 @@ import {Component} from 'angular2/core';
     <form (ngSubmit)="onSubmit(f)" #f="ngForm">
     <label for="mail">mail</label>
     <input ngControl="mail" type="text" id="mail" required #mail="ngForm">
-    <span class="error" *ngIf="!email.valid">not valid</span>
+    <span class="error" *ngIf="!mail.valid">not valid</span>
         <label for="password">pass</label>
-    <input ngControl="password" type="text" id="password" required>
+    <input ngControl="password" type="text" id="password" required #password="ngForm">
+     <span class="error" *ngIf="!password.valid">not valid</span>
         <label for="cpassword">confirm</label>
-    <input ngControl="cpassword" type="text" id="cpassword" required>
-    <button type="submit">submit</button>
+    <input ngControl="cpassword" type="text" id="cpassword" required #cpassword="ngForm">
+     <span class="error" *ngIf="!cpassword.valid">not valid</span>
+    <button type="submit" [disabled]="!f.valid||password.value!==cpassword.value">submit</button>
     </form>
+    {{user.mail}}{{user.password}}
     `
 })
 export class TemplateFormComponent{
-    onSubmit(value) {
-        console.log(value);
+    user={mail:'',password:''};
+    onSubmit(form) {
+        this.user.mail=form.value['mail'];
+        this.user.password=form.controls['password'].value;
     }
 }
